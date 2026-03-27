c47db87172b25ee0456e5155ab185281096337dd
Kshitij Saxena <kshitijsaxenaofficial@gmail.com>
2026-03-28 00:27:27 +0530

    initialize project with frontend and backend

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.send("InterviewIQ backend is running")
+})
+
+module.exports = app;
