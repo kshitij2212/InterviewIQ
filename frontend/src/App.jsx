0ba3912aa705c3df512827f3fbd1ea2ceabff8ae
Kshitij Saxena <kshitijsaxenaofficial@gmail.com>
2026-04-14 17:03:27 +0530

    refactor : update App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useAuthStore } from './store/authStore'
+import AppLayout from './components/layout/AppLayout'
+import ScrollToTop from './components/utils/ScrollToTop'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -8,6 +10,8 @@ import DashboardPage from './pages/DashboardPage'
 import InterviewSetup from './pages/InterviewSetup'
 import InterviewPage from './pages/InterviewPage'
 import ResultsPage from './pages/ResultsPage'
+import ProfilePage from './pages/ProfilePage'
+import HistoryPage from './pages/HistoryPage'
 
 export default function App() {
   const fetchUser = useAuthStore(s => s.fetchUser)
@@ -18,14 +22,19 @@ export default function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/"         element={<LandingPage />} />
+        <Route path="/login"    element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/interview/setup" element={<InterviewSetup />} />
-        <Route path="/interview/:id" element={<InterviewPage />} />
-        <Route path="/interview/:id/results" element={<ResultsPage />} />
+
+        <Route path="/interview/:id"         element={<InterviewPage />} />
+        <Route path="/interview/:id/results" element={<AppLayout><ResultsPage /></AppLayout>} />
+
+        <Route path="/dashboard"      element={<AppLayout><DashboardPage /></AppLayout>} />
+        <Route path="/interview/setup" element={<AppLayout><InterviewSetup /></AppLayout>} />
+        <Route path="/profile"        element={<AppLayout><ProfilePage /></AppLayout>} />
+        <Route path="/history"        element={<AppLayout><HistoryPage /></AppLayout>} />
       </Routes>
     </>
   )
